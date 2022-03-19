a8897100c0677793c9f78680452367ca256eecf2
Viney <vineykhaneja999@gmail.com>
2022-03-19 22:59:13 +0530

    [Viney] ADDED: Calculate Multiple Rides Fare & Min Fare Testing

--- Cab_Invoice_Tests/CabInvoiceProgramTests.cs
+++ Cab_Invoice_Tests/CabInvoiceProgramTests.cs
@@ -6,23 +6,32 @@ namespace Cab_Invoice_Tests
     public class Tests
     {
         CabInvoiceGenerator cabInvoiceGenerator;
+        Ride[] multipleRides;
         [SetUp]
         public void Setup()
         {
             cabInvoiceGenerator = new CabInvoiceGenerator();
+            multipleRides = new Ride[] { new Ride(2, 5), new Ride(3, 10),new Ride (0.1,0.5) };
         }
 
         [Test]
         public void GivenDistanceAndTimeForSingleRide_InvoiceGenerator_ReturnTotalFare()
         {
-            double result = cabInvoiceGenerator.GetTotalFare(5,10);
+            double result = cabInvoiceGenerator.GetTotalFareSingleRide(5,10);
             Assert.AreEqual(60,result);
         }
         [Test]
         public void GivenDistanceAndTimeForSingleRide_LessThanMinFare_InvoiceGenerator_ReturnMinimumFare()
         {
-            double result = cabInvoiceGenerator.GetTotalFare(0.1, 0.5);
+            double result = cabInvoiceGenerator.GetTotalFareSingleRide(0.1, 0.5);
             Assert.AreEqual(5, result);
         }
+        [Test]
+        public void GivenDistanceAndTimeForMultipleRide_InvoiceGenerator_ReturnTotalFare()
+        {
+            double result = cabInvoiceGenerator.GetTotalFareMultipleRide(multipleRides);
+            Assert.AreEqual(70, result);
+        }
+
     }
 }
