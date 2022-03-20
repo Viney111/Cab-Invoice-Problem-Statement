841cceb0df60e0df41f780190501db67a75113f5
Viney <vineykhaneja999@gmail.com>
2022-03-20 09:06:41 +0530

    [Viney] ADDED: Getting Enchanced Invoice Details

--- Cab_Invoice_Tests/CabInvoiceProgramTests.cs
+++ Cab_Invoice_Tests/CabInvoiceProgramTests.cs
@@ -1,17 +1,21 @@
 using NUnit.Framework;
 using Cab_Invoice_Problem;
+using System.Collections.Generic;
 
 namespace Cab_Invoice_Tests
 {
     public class Tests
     {
         CabInvoiceGenerator cabInvoiceGenerator;
+        List<EnhancedInvoiceDetails> enhancedInvoiceList;
         Ride[] multipleRides;
+
         [SetUp]
         public void Setup()
         {
             cabInvoiceGenerator = new CabInvoiceGenerator();
-            multipleRides = new Ride[] { new Ride(2, 5), new Ride(3, 10),new Ride (0.1,0.5) };
+            multipleRides = new Ride[] { new Ride(2, 10), new Ride(3, 10),new Ride (0.1,0.5),new Ride (0.1,0.5) };
+            enhancedInvoiceList = cabInvoiceGenerator.GetInvoiceDetailsOfRides(multipleRides);
         }
 
         [Test]
@@ -30,8 +34,16 @@ namespace Cab_Invoice_Tests
         public void GivenDistanceAndTimeForMultipleRide_InvoiceGenerator_ReturnTotalFare()
         {
             double result = cabInvoiceGenerator.GetTotalFareMultipleRide(multipleRides);
-            Assert.AreEqual(70, result);
+            Assert.AreEqual(80, result);
         }
-
+        [Test]
+        public void GivenDistanceAndTimeForMultipleRide_InvoiceGenerator_ReturnListofInvoiceDetails()
+        {
+            var details = enhancedInvoiceList[0];
+            double totalFare = details.totalFare; double avgFare = details.averageFarePerRide; int noOfRides = details.totalNumberOfRides;
+            Assert.AreEqual(80, totalFare);
+            Assert.AreEqual(20, avgFare);
+            Assert.AreEqual(4, noOfRides);
+        }
     }
 }
